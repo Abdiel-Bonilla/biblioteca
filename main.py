8a67c29e760304193f9dceb6980e10be5625e473
Abdiel-Bonilla <abboro123@gmail.com>
2024-07-05 10:42:34 -0600

    empezando con la base de datos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import psycopg2
 from flask import Flask,redirect,render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import PasswordField,StringField,SubmitField
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,4 +10,25 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('base.html')
-    
+
+@app.route('/libros')
+def libros():
+    #conectar con la BD
+    conexion= psycopg2.connect(
+        database= "biblioteca3a",
+        user="postgres",
+        password="cris20",
+        host="localhost",
+        port="5432"
+    )
+
+    #crear un cursor (objeto para recorrer las tablas)
+    cursor= conexion.cursor()
+    #ejecutar una consulta en postgres
+    cursor.execute('''SELECT*FROM libros''')
+    #recuperar informacion
+    datos= cursor.fetchall()
+    #cerrar cursor y conexion de la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('libros.html', datos=datos)
